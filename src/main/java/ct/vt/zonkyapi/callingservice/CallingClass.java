ff5f265bad5d2169ccef81702024fdd8148bf880
tallavla <vladimir.talla@tietoevry.com>
2023-01-16 08:41:34 +0100

    token refresh

--- src/main/java/ct/vt/zonkyapi/callingservice/CallingClass.java
+++ src/main/java/ct/vt/zonkyapi/callingservice/CallingClass.java
@@ -9,11 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CallingClass {
@@ -22,21 +25,24 @@ public class CallingClass {
 
     private URL url;
     private HttpURLConnection conn;
-    private String bearer = "Bearer 69364ffb-4fd8-4468-8f93-557277330914";
+    private String bearer = "Bearer 864449ae-a854-42b7-ba73-d49cdabd4dc6";
     private InputStreamReader in = null;
     private BufferedReader br = null;
 
 
     public List<ServiceLoan>  getZonkyLoans() throws Exception {
 
+            String bearer = getToken();
+
+
             url = new URL("https://api.zonky.cz/loans/marketplace");
             conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Authorization", bearer);
+            conn.setRequestProperty("Authorization", this.bearer);
             conn.setRequestMethod("GET");
             ZonkyResponseLoan[] zonkyResponse;
 
         int status = conn.getResponseCode();
-            log.info("Http_SC: " + status );
+            log.info("Get Zonky loans Http_SC: " + status );
 
             if (status > 299) {
                 in = new InputStreamReader(conn.getErrorStream());
@@ -75,5 +81,59 @@ public class CallingClass {
         //log.info("Response: " + content);
         return content;
     }
+
+    public String getToken() throws Exception {
+        String scope = "SCOPE_APP_BASIC_INFO SCOPE_INVESTMENT_READ SCOPE_INVESTMENT_WRITE SCOPE_NOTIFICATIONS_READ SCOPE_NOTIFICATIONS_WRITE";
+        String state = "123456789";
+        String clientId = "mujrobot";
+        String clientSecret = "mujrobot";
+        String responseType = "code";
+        String redirectUri = "https://app.zonky.cz/api/oauth/code";
+        String authUrl = "https://api.zonky.cz/oauth/authorize";
+        String accessTokenUrl = "https://api.zonky.cz/oauth/token";
+
+        url = new URL(authUrl);
+        conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("response_type", responseType);
+        parameters.put("client_id", clientId);
+        parameters.put("state", state);
+        parameters.put("scope", scope);
+        parameters.put("redirect_uri", redirectUri);
+
+        conn.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+        out.flush();
+        out.close();
+
+        int status = conn.getResponseCode();
+        log.info("OAuth call Http_SC: " + status );
+
+        String returnedRedirectUrl = conn.getHeaderFields().toString();
+        log.info(returnedRedirectUrl);
+
+        String content;
+
+        if (status > 299) {
+            in = new InputStreamReader(conn.getErrorStream());
+            content = getContent(in).toString().replace("\"","");
+            throw new ZonkyErrorResponseException(content);
+        } else {
+
+            in = new InputStreamReader(conn.getInputStream());
+            content = getContent(in).toString();
+
+        }
+        log.info(content);
+
+
+
+        return "Bearer 864449ae-a854-42b7-ba73-d49cdabd4dc6";
+    }
+
+
 
 }
